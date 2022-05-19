00e353d9bdc5ac587be8afa7a5cb97c9dde5e4ba
Vlad Tregubov <u.trehubau@gmail.com>
2022-05-19 23:11:28 +0200

    Fixes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,12 +6,6 @@ export const ROUTES = {
 	REGISTRATION: '/registration',
 	COURSES: '/courses',
 	COURSE_ADD: '/courses/add',
-
-	COURSE: (courseId = COURSE_ID) => {
-		return `/courses/${courseId}`;
-	},
-
-	COURSE_UPDATE: (courseId = COURSE_ID) => {
-		return `/courses/update/${courseId}`;
-	},
+	COURSE: (courseId = COURSE_ID) => `/courses/${courseId}`,
+	COURSE_UPDATE: (courseId = COURSE_ID) => `/courses/update/${courseId}`,
 };
